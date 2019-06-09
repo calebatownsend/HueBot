b8cc84e5beba13679249e436bce680d3ab1fbaeb
KRLacey <krlstuff@gmail.com>
2019-06-09 02:57:59 -0400

    Minor Cleanup
    minor code syntax and formatting cleanup

--- messageAnalyze.js
+++ messageAnalyze.js
@@ -7,28 +7,21 @@ let wordstore = JSON.parse(rawdata);
 rawdata = fs.readFileSync('channelstore.json');
 let channelstore = JSON.parse(rawdata);
 
-
-
 module.exports = {
     getPhraseforHotwords: function (message) {
         for (var key in wordstore) {
-            if (message.toLowerCase().includes(key)) 
-            {
+            if (message.toLowerCase().includes(key)) {
                 return wordstore[key][Math.floor(Math.random() * wordstore[key].length)]
             }
-
         }
         return null;
     },
 
-    getPhraseforChannel: function (channel)
-    {
+    getPhraseforChannel: function (channel) {
         for (var key in channelstore) {
-            if (channel.toLowerCase().includes(key)) 
-            {
+            if (channel.toLowerCase().includes(key)) {
                 return channelstore[key][Math.floor(Math.random() * channelstore[key].length)]
             }
-
         }
         return null;
     }
--- mybot.js
+++ mybot.js
@@ -15,7 +15,7 @@ const baseChannelReactionPercent = 1;
 
 var messageAnalyzer = require('./messageAnalyze.js');
 
-let emojiNames = ["donny","skwondering", "uhhuh", "thinking", "caleb", "santarich","josh","swiss", "jeremy","van","gray","chase","ray","toottoot","kevin","skno","skdrunk","tyler","ashley"];
+let emojiNames = ["donny","skwondering","uhhuh","thinking","caleb","santarich","josh","swiss","jeremy","van","gray","chase","ray","toottoot","kevin","skno","skdrunk","tyler","ashley"];
 let emojis = [];
 
 const fs = require('fs');
@@ -34,81 +34,83 @@ client.on("message", (message) => {
   //load emoji objects if necessary
   if (emojis.length == 0 && message.guild.name == "League Friends") {
     emojiNames.forEach(function (name) {
-      var thisEmoji =message.guild.emojis.find(emoji => emoji.name === name);
+      var thisEmoji = message.guild.emojis.find(emoji => emoji.name === name);
       if (thisEmoji !=  null)
-      emojis.push(thisEmoji);
+        emojis.push(thisEmoji);
     });
-
   }
 
+  var date = new Date();
+  var today = date.getDate();
 
-var d = new Date();
-var today = d.getDate();
-
-//reset the message counts if it's the next day
-if (today != dibstore["date"]) {
-  for (var key in dibstore) {
-    if (key != "date") {
-      var obj = dibstore[key];
-      obj["messages"] = 0;
+  //reset the message counts if it's the next day
+  if (today != dibstore["date"]) {
+    for (var key in dibstore) {
+      if (key != "date") {
+        var obj = dibstore[key];
+        obj["messages"] = 0;
+      }
     }
+    dibstore["date"] = today;
   }
-  dibstore["date"] = today;
-}
 
-//check if new user
-if (dibstore[message.author.id] != null) {
-  dibstore[message.author.id]["messages"]++;
-}
-else {
-  dibstore[message.author.id] = { "messages": 0, "username": message.author.username }
-}
-if (!dibstore[message.author.id]["username"]) { dibstore[message.author.id]["username"] = message.author.username; }
+  var userid = message.author.id;
 
-//back to work responses
-if (dibstore[message.author.id]["messages"] == 200 && d.getHours() < 18) {
-  reply(message, "TWO HUNDRED MESSAGES TODAY. getting any work done??");
-}
-else if (dibstore[message.author.id]["messages"] == 100 && d.getHours() < 18) {
-  reply(message, "does your boss know you are on discord? hue");
-}
-else if (dibstore[message.author.id]["messages"] == 50 && d.getHours() < 18) {
-  reply(message, "you've been on Discord a lot today. Taking a long lunch?");
-}
-
-
-else {
-  var response = messageAnalyzer.getPhraseforHotwords(message.content);
-  if (response != null) {
-    if (rollPercent(response.probabilityModifier + baseHotwordResponsePercent)) {
-      if (response.responseType == "send")
-        send(message, response.phrase);
-      else
-        reply(message, response.phrase);
-    }
+  //check if new user
+  if (dibstore[userid] == null) {
+    dibstore[userid] = { "messages": 1, "username": message.author.username }
   }
-  response = messageAnalyzer.getPhraseforChannel(message.channel.name);
-  {
+  else {
+    dibstore[userid]["messages"]++;
+  }
+
+  //check if username exists for user
+  if (!dibstore[userid]["username"]) {
+    dibstore[userid]["username"] = message.author.username;
+  }
+
+  //back to work responses
+  if (dibstore[userid]["messages"] == 200 && date.getHours() < 18) {
+    reply(message, "TWO HUNDRED MESSAGES TODAY. getting any work done??");
+  }
+  else if (dibstore[userid]["messages"] == 100 && date.getHours() < 18) {
+    reply(message, "does your boss know you are on discord? hue");
+  }
+  else if (dibstore[userid]["messages"] == 50 && date.getHours() < 18) {
+    reply(message, "you've been on Discord a lot today. Taking a long lunch?");
+  }
+  else {
+    var response = messageAnalyzer.getPhraseforHotwords(message.content);
     if (response != null) {
-      if (rollPercent(response.probabilityModifier + baseChannelResponsePercent)) {
+      if (rollPercent(response.probabilityModifier + baseHotwordResponsePercent)) {
         if (response.responseType == "send")
           send(message, response.phrase);
         else
           reply(message, response.phrase);
       }
     }
+    response = messageAnalyzer.getPhraseforChannel(message.channel.name);
+    {
+      if (response != null) {
+        if (rollPercent(response.probabilityModifier + baseChannelResponsePercent)) {
+          if (response.responseType == "send")
+            send(message, response.phrase);
+          else
+            reply(message, response.phrase);
+        }
+      }
+    }
+    if (rollPercent(baseChannelReactionPercent)) {
+      message.react(emojis[Math.floor(Math.random() * emojis.length)]);
+    }
   }
-  if (rollPercent(baseChannelReactionPercent)) {
-    message.react(emojis[Math.floor(Math.random() * emojis.length)]);
-  }
-}
-//save message numbers
-fs.writeFileSync('dibstore.json', JSON.stringify(dibstore));
+  
+  //save message numbers
+  fs.writeFileSync('dibstore.json', JSON.stringify(dibstore));
 });
 
 //Connect to Discord
 client.login(config.token);
-
 
 //SUPPORT FUNCTIONS:
 function rollPercent(percent) {
