74adf2590659ce6a06c09252a4292d90005e802a
Caleb Townsend <calebatownsend@gmail.com>
2019-06-09 18:37:41 -0400

    increased reaction chance. more words and phrases.

--- mybot.js
+++ mybot.js
@@ -4,14 +4,14 @@ const config = require('./config.json');
 
 //RESPONSE PERCENTAGES:
 //The percent that HueBot will respond if trigger is found.
-const baseHotwordResponsePercent = 12;
+const baseHotwordResponsePercent = 15;
 
 //The percent that HueBot will respond with a general phrase. 
 //Needs to be much lower than Hotword response because this is against every message in the channel.
 const baseChannelResponsePercent = 1;
 
 //The percent that HueBot will react to a given message
-const baseChannelReactionPercent = 1;
+const baseChannelReactionPercent = 2;
 
 var messageAnalyzer = require('./messageAnalyze.js');
 
