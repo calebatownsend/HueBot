6f3ea84b35df933e29bdd5c2e34a3e9099ee5b23
KRLacey <krlstuff@gmail.com>
2019-12-01 18:57:52 -0500

    Removed obsolete const
    Removed lfGuildID which was made obsolete after emoji setup moved to botInterface init

--- mybot.js
+++ mybot.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require('./config.json');
-const lfGuildID = "144653611819859969"; // "League Friends" server ID
 
 var util = {
   interfaces: {
